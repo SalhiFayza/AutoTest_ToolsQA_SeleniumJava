aa708c6aa4b6ef6615a01d3617c548038d97d61c
SalhiFayza <fayzasalhif@gmail.com>
2023-08-28 15:51:01 +0100

    Add class CheckBox

---
+++ src/test/java/com/testSelenium/CheckBox.java
@@ -0,0 +1,41 @@
+package com.testSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CheckBox {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://demoqa.com/checkbox/");
+		
+		WebElement checkbox;
+		
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		
+		checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"tree-node\"]/ol/li/span/label/span[1]")));
+		
+		// Check the checkbox if it is not selected
+		if (!checkbox.isSelected()) {
+			
+		checkbox.click();
+		
+		}
+		
+	   
+		
+	}
+
+}
